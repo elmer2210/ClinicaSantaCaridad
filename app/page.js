a387376ea3bffeeb29ec3b41018e26e57c1293b6
Elmer Rivadeneira <elmerrivadeneira@gmail.com>
2023-10-10 20:16:56 -0500

    Intentando conectar a strapi

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 "use client";
-
 import React from 'react';
 import NavbarComponent from '@/components/Narbar/Narbar.component';
 import FooterComponent from '@/components/Footer/Footer.component';
@@ -11,7 +10,18 @@ import ContactSection from '@/components/Contact/Contactus.component';
 import HeaderComponent from '@/components/Header/Header.component';
 
 
-export default function Home() {
+export async function generateStaticParams() {
+  const data = await fetch('http://localhost:1337/api/carousels?populate[image][fields][0]=url')
+  .then((res) => res.json())
+  console.log(data)
+  return data.map((data) =>{
+    data:data
+  })
+}
+
+
+export default function Home({ params }) {
+  console.log(params)
   return (
     <main>
       <NavbarComponent />
--- components/Carousel/Carousel.component.jsx
+++ components/Carousel/Carousel.component.jsx
@@ -1,8 +1,9 @@
 // components/Carousel.js
-import React, {Component} from 'react';
+
+import React, {Component, useEffect} from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import '@/assets/css/carousel.style.css'
+import '@/assets/css/carousel.style.css';
 
 function CarouselComponent() {
 
@@ -12,22 +13,22 @@ function CarouselComponent() {
         'humberto-chavez-FVh_yqLR9eA-unsplash.jpg',
         'usman-yousaf-pTrhfmj2jDA-unsplash.jpg'
     ];
-        return (
-                <Carousel 
-                    autoPlay 
-                    dynamicHeight 
-                    infiniteLoop 
-                    showIndicators={false} 
-                    showThumbs={false}
-                >
-                    {imagesFiles.map((filename, index) => (
-                        <div key={index}>
-                            <img src={`/images/${filename}`} alt={`Imagen ${index + 1}`} />
-                        </div>
-                    ))}
-                </Carousel>
+    return (
+            <Carousel 
+                autoPlay 
+                dynamicHeight 
+                infiniteLoop 
+                showIndicators={false} 
+                showThumbs={false}
+            >
+                {imagesFiles.map((filename, index) => (
+                    <div key={index}>
+                        <img src={`/images/${filename}`} alt={`Imagen ${index + 1}`} />
+                    </div>
+                ))}
+            </Carousel>
 
-        );
+    );
 }
 
 export default CarouselComponent;
